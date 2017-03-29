48eff1fddae32c2f170b2778b10a4c04fe476df8
lovieira <luk14236@gmail.com>
2017-03-29 00:38:54 -0300

    enviando as continuacoes da forma de pagamento

--- www/js/models.js
+++ www/js/models.js
@@ -447,18 +447,18 @@ game7App.factory("Cliente", function (Ajax,$http) {
         )
     };
     obj.get_cliente = function () {
-navigator.geolocation.getCurrentPosition(function(position)
-{
-    // just to show how to access latitute and longitude
-    var location = [position.coords.latitude, position.coords.longitude];
-    alert(location);
-},
-function(error)
-{
-    // error getting GPS coordinates
-    alert('code: ' + error.code + ' with message: ' + error.message + '\n');
-},
-{ enableHighAccuracy: true, maximumAge: 3000, timeout: 5000 });
+//navigator.geolocation.getCurrentPosition(function(position)
+//{
+//    // just to show how to access latitute and longitude
+//    var location = [position.coords.latitude, position.coords.longitude];
+//    alert(location);
+//},
+//function(error)
+//{
+//    // error getting GPS coordinates
+//    alert('code: ' + error.code + ' with message: ' + error.message + '\n');
+//},
+//{ enableHighAccuracy: true, maximumAge: 3000, timeout: 5000 });
         //Get relação de clientes
         var url = URL_BASE + "clientes";
         var params = {
